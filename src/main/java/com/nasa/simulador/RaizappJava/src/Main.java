be2f9e7bc243190873155a96242872db78a75312
jesus <jesuspolanco1502@gmail.com>
2026-07-08 10:43:22 -0400

    entregable3-4

--- src/main/java/com/nasa/simulador/RaizappJava/src/Main.java
+++ src/main/java/com/nasa/simulador/RaizappJava/src/Main.java
@@ -1,5 +1,23 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hola Mundo");
+package com.nasa.simulador;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("ARTEMIS II MISSION SIMULATOR");
+        System.out.println("=================================");
+
+        try {
+
+            OrekitConfig.init();
+
+            TLIPrototype.run();
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+
+        }
     }
 }
